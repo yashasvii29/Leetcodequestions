0f60b365d9473f614afd39d09333ecd262ea8273
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-06-05 13:39:03 +0530

    Time: 10 ms (36.83%), Space: 43.5 MB (37.64%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] finalFreq = new int[26];
+        Arrays.fill(finalFreq, Integer.MAX_VALUE);
+
+        for (String word : words) {
+            int[] curFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                curFreq[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                finalFreq[i] = Math.min(finalFreq[i], curFreq[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            // Get the character corresponding to the current index
+            char currentChar = (char) (i + 'a');
+            System.out.println(currentChar);
+            
+            // Add the character to the result list as many times as it appears in all words
+            for (int j = 0; j < finalFreq[i]; j++) {
+                ans.add(String.valueOf(currentChar));
+            }
+        }
+        return ans;
+    }
+}
