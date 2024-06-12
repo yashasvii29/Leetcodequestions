b9f34a99d23102ac607006407a693c922c1d177a
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-06-12 22:06:48 +0530

    Time: 5 ms (24.21%), Space: 44.6 MB (99.12%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                // If there's a mismatch, try removing one character either from the beginning  i or from the ending j
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true; // The string is already a palindrome
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
