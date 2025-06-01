e5b38a371fbc93736827b2fa8c1378631b49b047
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2025-06-01 13:15:46 +0530

    Time: 32 ms (56.2%), Space: 42.3 MB (72.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,3 +1,31 @@
+// /this is a brute force approach
+
+// class Solution {
+//     public boolean canPartition(int[] arr) {
+
+//         // this que is same as split array
+//         return Printsubset1(arr,0,0,0);
+//     }
+//     public static boolean Printsubset1(int arr[], int i,int sum1,int sum2){
+        
+     
+//         if (i == arr.length) {
+//             return sum1 == sum2;
+//         }
+
+//         // Option 1: Add arr[i] to subset2
+//         boolean without = Printsubset1(arr, i + 1, sum1, sum2 + arr[i]);
+
+//         // Option 2: Add arr[i] to subset1
+//         boolean with = Printsubset1(arr, i + 1, sum1 + arr[i], sum2);
+
+//         return with || without;
+//     }
+// }
+
+
+// optimize code
+
 class Solution {
     public boolean canPartition(int[] nums) {
            int totalSum = 0;
