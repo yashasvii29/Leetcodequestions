95abc4ed613a35abb1ab985545d0b98986819e41
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2025-07-15 19:18:38 +0530

    Time: 5 ms (85.07%), Space: 45.8 MB (6.15%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,33 @@
+
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int a : asteroids) {
+            while (!st.isEmpty() && a < 0 && st.peek() > 0) {
+                int sum = a + st.peek();
+                if (sum < 0) {
+                    st.pop();
+                } else if (sum > 0) {
+                    a = 0;
+                    break;
+                } else { // sum == 0
+                    st.pop();
+                    a = 0;
+                }
+            }
+
+            if (a != 0) {
+                st.push(a);
+            }
+        }
+
+        int[] result = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            result[i] = st.pop();
+        }
+
+        return result;
+    }
+}
