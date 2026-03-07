3fac260bc7a6b3ea97879ac1d32b6d9c2d7b6420
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2026-03-07 19:57:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,5 +1,36 @@
 class Solution {
 
+    // This problem Max Area of Island is almost the same as Number of Islands, but instead of counting islands, we calculate the size (area) of each island and return the maximum.
+
+
+//     Simple Logic
+
+// Traverse the grid.
+
+// If we find 1 and it is not visited, start DFS.
+
+// DFS will explore the whole island.
+
+// While exploring, count the number of cells.
+
+// Keep track of the maximum area.
+
+// So instead of:
+
+// count islands
+
+// we do:
+
+// calculate island area
+// maxArea = max(maxArea, currentArea)
+// Key Idea
+
+// Each DFS call returns:
+
+// area = 1 + area of neighbours
+
+// because the current cell itself counts as 1 unit area.
+
     int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
     public int maxAreaOfIsland(int[][] grid) {
