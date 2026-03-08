16927bd1470c1934aa1c5ecbdf886eb616f7ecaa
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2026-03-08 22:49:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,66 +1,61 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				if (grid[i][j] == 2) {
-					q.add(new Pair(i, j, 0));
-				}
+        // use bfs traversal because hume har node ke immediate neighbours ko rotten krna h means adjacent neighbours ko
+        // if any single fresh orange is left in grid then we will return -1
+        // we have to return the minimum no of minutes to convert all the fresh oranges to rottm oranges
+        // grid m we can have multiple rotten oranges means 2 which is our source node from where we have to start to make all the adjacent fresh oranges to rotten (means we have to convert adjacent fresh oranges to rotten oranges 1 to 2) so in this que we will use Multi source BFS
 
-			}
-		}
+       if (grid == null && grid.length == 0)
+            return -1;
 
-		if (q.isEmpty()) {
-			for (int i = 0; i < grid.length; i++) {
-				for (int j = 0; j < grid[0].length; j++) {
-					if (grid[i][j] == 1) {
-						return -1;
-					}
-				}
-			}
-			return 0;
-		}
-		int r[] = { 1, -1, 0, 0 };
-		int c[] = { 0, 0, 1, -1 };
-		int ans = 0;
-		while (!q.isEmpty()) {
-			Pair rp = q.remove();
-			ans = rp.time;
-			for (int i = 0; i < c.length; i++) {
-				if (rp.i + r[i] < 0 || rp.i + r[i] >= grid.length || rp.j + c[i] < 0 || rp.j + c[i] >= grid[0].length) {
-					continue;
-				}
-				int x = rp.i + r[i];
-				int y = rp.j + c[i];
-				if (grid[x][y] == 1) {
-					grid[x][y]=2;// add na ho next time
-					q.add(new Pair(x, y, rp.time + 1));
-				}
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int freshOranges = 0;
 
-			}
+        Queue<int[]> q = new LinkedList<>();
 
-		}
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				if (grid[i][j] == 1) {
-					return -1;
-				}
-			}
-		}
-		return ans;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    freshOranges++;
+                } else if (grid[i][j] == 2) {
+                    q.offer(new int[] { i, j });
+                }
+            }
+        }
 
-	}
+        if (freshOranges == 0)
+            return 0;
 
-	public class Pair {
-		int i;
-		int j;
-		int time;
+        int minutes = 0;
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // up, down, left, right
 
-		public Pair(int i, int j, int time) {
-		
-			this.i = i;
-			this.j = j;
-			this.time = time;
-		}
-	}
+        while (!q.isEmpty()) {
+            int qsize = q.size();
+
+            // level of bfs - look out for neighbours
+            for (int i = 0; i < qsize; i++) {
+                int[] curr = q.poll();
+
+                for (int[] dir : directions) {
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+
+                    // check for bounds
+                    if (x >= 0 && y >= 0 && x < rows && y < cols && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        freshOranges--;
+                        q.offer(new int[] { x, y });
+                    }
+                }
+            }
+
+            // count level of bfs
+            minutes++;
+        }
+
+     // for the last level of unecessary bfs level
+        return freshOranges == 0 ? minutes - 1 : -1;
+    
+    }
 }
