3c7a7b294f7a0ec23806e925b0b6a03f2cfff845
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2026-03-20 22:11:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,5 +1,6 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
+        // we have to only visited the cells which are 0 so visisted ka track rakhne ke liye uss cell ko 1 mark kr denge after visiting it
          int n = grid.length;
         if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         
@@ -18,7 +19,7 @@ class Solution {
             for (int[] d : dirs) {
                 int nr = r + d[0], nc = c + d[1];
                 if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0) {
-                    grid[nr][nc] = 1;
+                    grid[nr][nc] = 1; // mark visisted
                     queue.offer(new int[]{nr, nc, dist + 1});
                 }
             }
