7029734fd8d0de1bf3033e478cc6257139a5bba0
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2026-03-21 09:48:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -9,31 +9,64 @@ class Solution {
         boolean[][] pacific  = new boolean[rows][cols];
         boolean[][] atlantic = new boolean[rows][cols];
         
+          // Start DFS from Pacific borders (top row + left col)
         for (int i = 0; i < rows; i++) {
-            dfs(heights, pacific,  i, 0);
-            dfs(heights, atlantic, i, cols-1);
+            dfs(heights, pacific,  i, 0);        // left column
+            dfs(heights, atlantic, i, cols - 1); // right column
         }
+
+        // Start DFS from Atlantic borders (bottom row + right col)
         for (int j = 0; j < cols; j++) {
-            dfs(heights, pacific,  0, j);
-            dfs(heights, atlantic, rows-1, j);
+            dfs(heights, pacific,  0, j);        // top row
+            dfs(heights, atlantic, rows - 1, j); // bottom row
         }
-        
+
+        // Collect cells reachable by BOTH oceans
         List<List<Integer>> result = new ArrayList<>();
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < cols; c++)
                 if (pacific[r][c] && atlantic[r][c])
                     result.add(Arrays.asList(r, c));
-        
+
         return result;
     }
-    
+
     private void dfs(int[][] heights, boolean[][] visited, int r, int c) {
-        visited[r][c] = true;
+        visited[r][c] = true; // mark this cell reachable from ocean
+
         for (int[] d : dirs) {
-            int nr = r + d[0], nc = c + d[1];
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            // Move to neighbor only if:
+            // 1. Inside bounds
+            // 2. Not already visited
+            // 3. Height >= current (reverse flow = going uphill)
             if (nr >= 0 && nr < rows && nc >= 0 && nc < cols
-                    && !visited[nr][nc] && heights[nr][nc] >= heights[r][c])
+                    && !visited[nr][nc]
+                    && heights[nr][nc] >= heights[r][c]) {
                 dfs(heights, visited, nr, nc);
-        }
+            }
     }
     }
+}
+    
+
+
+// Given a grid of heights, water can flow to a neighbor (up, down , left and right )
+// if neighbor height <= current height
+
+// Pacific Ocean  touches TOP and LEFT borders
+// Atlantic Ocean touches BOTTOM and RIGHT borders
+
+// Find all cells from which water can reach BOTH oceans
+
+
+
+// Given a grid of heights, water can flow to a neighbor
+// if neighbor height <= current height
+
+// Pacific Ocean  touches TOP and LEFT borders
+// Atlantic Ocean touches BOTTOM and RIGHT borders
+
+// Find all cells from which water can reach BOTH oceans
